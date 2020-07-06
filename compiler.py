bca0047bada77ee7bb4f9da7961b1a2c0cd233a0
Ilhee <asahi8769@gmail.com>
2020-07-06 10:48:18 +0900

    Add all my files

--- compiler.py
+++ compiler.py
@@ -10,6 +10,11 @@ if __name__ == "__main__":
     dir_loc = os.path.join(os.getcwd(), 'dist')
     file_to_compile = path_find('Main_Window.py', r'D:/')
     file_to_compile = file_to_compile.replace('\\', '/')
-    install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd {file_to_compile}'
+    icon_name = r'D:\devs\consult2\flying_bird.ico'.replace('\\', '/')
+
+    # install_command = f'pyinstaller.exe -F --noconsole --hidden-import=matplotlib.pyplot --hidden-import=sqlite3 --hidden-import=pandas --hidden-import=tqdm --hidden-import=smtplib --hidden-import=email.mime.image --hidden-import=email.mime.text --hidden-import=email.mime.multipart --hidden-import=email.mime.application {file_to_compile}'
+    install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
+    print(install_command)
+
     subprocess_cmd(f'cd {dir_venv_64} & {install_command} & cd dist & copy Main_Window.exe {dir_loc}')
     packaging(r'Main_Window.exe', 'Cookies', 'Images', 'Main_DB', 'Objections', 'Spawn', 'Cookies_objection')

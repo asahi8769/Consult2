7479ae8a0ba48af70cec8bf81fbd0af50fda446a
Ilhee <asahi8769@gmail.com>
2020-07-06 10:58:16 +0900

    Add all my files

--- compiler.py
+++ compiler.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     file_to_compile = file_to_compile.replace('\\', '/')
     icon_name = r'D:\devs\consult2\flying_bird.ico'.replace('\\', '/')
 
-    install_command = f'pyinstaller.exe -F --hidden-import=matplotlib.pyplot --hidden-import=sqlite3 --hidden-import=pandas --hidden-import=tqdm --hidden-import=smtplib --hidden-import=email.mime.image --hidden-import=email.mime.text --hidden-import=email.mime.multipart --hidden-import=email.mime.application --icon={icon_name} {file_to_compile}'
-    # install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
+    # install_command = f'pyinstaller.exe -F --noconsole --hidden-import=matplotlib.pyplot --hidden-import=sqlite3 --hidden-import=pandas --hidden-import=tqdm --hidden-import=smtplib --hidden-import=email.mime.image --hidden-import=email.mime.text --hidden-import=email.mime.multipart --hidden-import=email.mime.application --icon={icon_name} {file_to_compile}'
+    install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
     print(install_command)
 
     subprocess_cmd(f'cd {dir_venv_64} & {install_command} & cd dist & copy Main_Window.exe {dir_loc}')

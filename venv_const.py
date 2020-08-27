1d1e7ff7fbb57a4345ae75ccaa2bf0e9d0a225ca
Ilhee <asahi8769@gmail.com>
2020-08-27 14:15:44 +0900

    Apply all changes

--- venv_const.py
+++ venv_const.py
@@ -8,4 +8,13 @@ def install(lib):
     return f'pip --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org install {lib}'
 
 
-subprocess_cmd(f'cd {dir_venv_64} & {install("tqdm")} & {install("openpyxl")} & {install("matplotlib")} & {install("pyinstaller")} & {install("xlrd")} & {install("pandas")} & {install("pyautogui")}')
+subprocess_cmd(f'cd {dir_venv_64} & '
+               # f'{install("tqdm")} & '
+               # f'{install("openpyxl")} & '
+               # f'{install("matplotlib")} & '
+               # f'{install("pyinstaller")} & '
+               # f'{install("xlrd")} & '
+               # f'{install("pandas")} & '
+               # f'{install("pyautogui")}'
+               f'{install("pulp")} & '
+               )

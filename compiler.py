a94769a89ce741e8d7bf673086a550d316d3e01d
Ilhee <asahi8769@gmail.com>
2020-09-10 14:14:46 +0900

    Apply all changes

--- compiler.py
+++ compiler.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     file_to_compile = file_to_compile.replace('\\', '/')
     icon_name = os.path.join(os.getcwd(), 'flying_bird.ico').replace('\\', '/')
     # install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
-    install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd {file_to_compile}'
-    # install_command = f'pyinstaller.exe -F --hidden-import=xlrd {file_to_compile}'
+    # install_command = f'pyinstaller.exe -F --noconsole --hidden-import=xlrd {file_to_compile}'
+    install_command = f'pyinstaller.exe -F --hidden-import=xlrd {file_to_compile}'
     print(install_command)
 
     subprocess_cmd(f'cd {dir_venv_64} & {install_command} & cd dist & copy Main_Window.exe {dir_loc}')
